1c3f15b9a9c167f0e81e340939d9b2dc0b378925
Boran <borancumaklar@icloud.com>
2025-09-06 23:07:27 +0300

    get put delete

--- rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -2,29 +2,45 @@ package kodlama.io.rentACar.webApi.controllers;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController //annotation
 @RequestMapping("/api/brands")
+@AllArgsConstructor
 public class BrandsController {
     private BrandService brandService;
 
-    @Autowired
-    public BrandsController(BrandService brandService) {
-        this.brandService = brandService;
-    }
-
-    @GetMapping("/getall")
+    @GetMapping()
     public List<GetAllBrandsResponse> getAll(){
         return brandService.getAll();
     }
 
-    @PostMapping("/add")
-    public void add(@RequestBody CreateBrandRequest createBrandRequest) {
+    @GetMapping("/{id}")
+    public GetByIdBrandResponse getById(@PathVariable int id){
+        return brandService.getById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(code= HttpStatus.CREATED)
+    public void add(@RequestBody() CreateBrandRequest createBrandRequest) {
         this.brandService.add(createBrandRequest);
+    }
+
+    @PutMapping
+    public void update(@RequestBody() UpdateBrandRequest updateBrandRequest) {
+        this.brandService.update(updateBrandRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        this.brandService.delete(id);
     }
 }
